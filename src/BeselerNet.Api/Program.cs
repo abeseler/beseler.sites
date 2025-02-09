d2082ae6bce880f0c12b3c09157a5de803345ef1
Adam Beseler <adam.beseler@gmail.com>
2025-02-09 17:37:17 -0600

    added fusion cache

--- src/BeselerNet.Api/Program.cs
+++ src/BeselerNet.Api/Program.cs
@@ -4,6 +4,12 @@ using BeselerNet.Api.Core;
 using BeselerNet.Api.OAuth;
 using BeselerNet.Api.Webhooks;
 using Dapper;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using ZiggyCreatures.Caching.Fusion;
+using ZiggyCreatures.Caching.Fusion.Backplane.StackExchangeRedis;
+using ZiggyCreatures.Caching.Fusion.Serialization.SystemTextJson;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +20,23 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddRequestTimeouts();
 builder.Services.AddProblemDetails();
 builder.Services.AddOpenApi();
+
+builder.Services.AddMemoryCache();
+builder.Services.AddFusionCache()
+    .WithOptions(o =>
+    {
+        o.CacheKeyPrefix = $"{builder.Environment.ApplicationName}:";
+    })
+    .WithSerializer(new FusionCacheSystemTextJsonSerializer())
+    .WithDistributedCache(new RedisCache(new RedisCacheOptions() { Configuration = builder.Configuration["ConnectionStrings:Cache"] }))
+    .WithBackplane(
+        new RedisBackplane(new RedisBackplaneOptions() { Configuration = builder.Configuration["ConnectionStrings:Cache"] }))
+    .AsHybridCache();
+
+builder.Services.AddOpenTelemetry()
+  .WithTracing(tracing => tracing.AddFusionCacheInstrumentation())
+  .WithMetrics(metrics => metrics.AddFusionCacheInstrumentation());
+
 builder.Services.AddHostedService<StartupService>();
 
 builder.Services.AddScoped<Cookies>();
--- src/BeselerNet.Api/StartupService.cs
+++ src/BeselerNet.Api/StartupService.cs
@@ -1,13 +1,20 @@
 ﻿using Beseler.ServiceDefaults;
+using Microsoft.Extensions.Caching.Hybrid;
 
 namespace BeselerNet.Api;
 
-internal sealed class StartupService(StartupHealthCheck startupCheck, ILogger<StartupService> logger) : BackgroundService
+internal sealed class StartupService(HybridCache cache, StartupHealthCheck startupCheck, ILogger<StartupService> logger) : BackgroundService
 {
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        await cache.GetOrCreateAsync("Startup:Api", async entry =>
+        {
+            await Task.CompletedTask;
+            return true;
+        }, cancellationToken: stoppingToken);
+        await cache.RemoveAsync("Startup:Api", stoppingToken);
+
         logger.LogInformation("The startup service has completed.");
         startupCheck.StartupCompleted = true;
-        return Task.CompletedTask;
     }
 }
