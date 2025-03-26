7649407897fe3a1a279b870fda04452b4db2e784
Adam Beseler <adam.beseler@gmail.com>
2025-03-26 00:03:56 -0500

    updated healthcheck response writing

--- src/Beseler.ServiceDefaults/Extensions.cs
+++ src/Beseler.ServiceDefaults/Extensions.cs
@@ -1,3 +1,4 @@
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Http;
@@ -36,19 +37,17 @@ public static class Extensions
     {
         app.MapHealthChecks("/_health", new HealthCheckOptions()
         {
-            ResponseWriter = WriteResponse
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
         }).CacheOutput("HealthCheck").WithRequestTimeout(TimeSpan.FromSeconds(10));
 
         app.MapHealthChecks("/_alive", new HealthCheckOptions()
         {
-            Predicate = r => r.Tags.Contains("live"),
-            ResponseWriter = WriteResponse
+            Predicate = r => r.Tags.Contains("live")
         });
 
         app.MapHealthChecks("/_ready", new HealthCheckOptions()
         {
-            Predicate = r => r.Tags.Contains("ready"),
-            ResponseWriter = WriteResponse
+            Predicate = r => r.Tags.Contains("ready")
         });
 
         app.MapGet("/coffee", () => TypedResults.Text("I'm a teapot!", statusCode: StatusCodes.Status418ImATeapot))
@@ -181,52 +180,6 @@ public static class Extensions
         builder.Services.AddHealthChecks().AddCheck<StartupHealthCheck>("ready", tags: ["ready"]);
         builder.Services.AddHealthChecks().AddCheck("self", () => HealthCheckResult.Healthy(), tags: ["live"]);
 
-
         return builder;
-    }
-
-    private static Task WriteResponse(HttpContext context, HealthReport healthReport)
-    {
-        context.Response.ContentType = "application/json; charset=utf-8";
-
-        var options = new JsonWriterOptions { Indented = true };
-
-        using var memoryStream = new MemoryStream();
-        using (var jsonWriter = new Utf8JsonWriter(memoryStream, options))
-        {
-            jsonWriter.WriteStartObject();
-            jsonWriter.WriteString("status", healthReport.Status.ToString());
-            jsonWriter.WriteString("duration", healthReport.TotalDuration.ToString());
-            jsonWriter.WriteStartObject("results");
-
-            foreach (var healthReportEntry in healthReport.Entries)
-            {
-                jsonWriter.WriteStartObject(healthReportEntry.Key);
-                jsonWriter.WriteString("status",
-                    healthReportEntry.Value.Status.ToString());
-                jsonWriter.WriteString("duration",
-                    healthReportEntry.Value.Duration.ToString());
-                jsonWriter.WriteString("description",
-                    healthReportEntry.Value.Description);
-                jsonWriter.WriteStartObject("data");
-
-                foreach (var item in healthReportEntry.Value.Data)
-                {
-                    jsonWriter.WritePropertyName(item.Key);
-
-                    JsonSerializer.Serialize(jsonWriter, item.Value,
-                        item.Value?.GetType() ?? typeof(object));
-                }
-
-                jsonWriter.WriteEndObject();
-                jsonWriter.WriteEndObject();
-            }
-
-            jsonWriter.WriteEndObject();
-            jsonWriter.WriteEndObject();
-        }
-
-        return context.Response.WriteAsync(
-            Encoding.UTF8.GetString(memoryStream.ToArray()));
     }
 }
