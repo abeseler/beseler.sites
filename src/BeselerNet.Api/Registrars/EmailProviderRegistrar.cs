507262d2cb0397c29b8f20598aed4b49174bdc3b
Adam Beseler <adam.beseler@gmail.com>
2025-09-06 22:14:56 -0500

    disable emailing via sendgrid

--- src/BeselerNet.Api/Registrars/EmailProviderRegistrar.cs
+++ src/BeselerNet.Api/Registrars/EmailProviderRegistrar.cs
@@ -9,22 +9,22 @@ internal static class EmailProviderRegistrar
     public static IHostApplicationBuilder AddEmailProviders(this IHostApplicationBuilder builder)
     {
         builder.Services.AddScoped<EmailerProvider>();
-        builder.AddSendGrid();
+        //builder.AddSendGrid();
         builder.AddMailjet();
 
         return builder;
     }
 
-    private static void AddSendGrid(this IHostApplicationBuilder builder)
-    {
-        builder.Services.AddOptions<SendGridOptions>().BindConfiguration(SendGridOptions.SectionName);
-        builder.Services.AddScoped<SendGridEmailService>();
-        builder.Services.AddSendGrid(options =>
-        {
-            var key = builder.Configuration.GetValue<string>("SendGrid:ApiKey");
-            options.ApiKey = string.IsNullOrWhiteSpace(key) ? "MissingApiKey" : key;
-        });
-    }
+    //private static void AddSendGrid(this IHostApplicationBuilder builder)
+    //{
+    //    builder.Services.AddOptions<SendGridOptions>().BindConfiguration(SendGridOptions.SectionName);
+    //    builder.Services.AddScoped<SendGridEmailService>();
+    //    builder.Services.AddSendGrid(options =>
+    //    {
+    //        var key = builder.Configuration.GetValue<string>("SendGrid:ApiKey");
+    //        options.ApiKey = string.IsNullOrWhiteSpace(key) ? "MissingApiKey" : key;
+    //    });
+    //}
 
     private static void AddMailjet(this IHostApplicationBuilder builder)
     {
